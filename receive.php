a94023b71f67729f3299e2640f790148c44feae9
yujinago <yuji.yuji.0813@gmail.com>
2023-11-04 17:15:46 +0900

    fix_match

--- receive.php
+++ receive.php
@@ -9,18 +9,16 @@
     // print_r($_POST);
     echo htmlspecialchars($_POST['recipe_name'], ENT_QUOTES);
     echo '<br>';
-    if ($_POST['category'] == '1') echo '和食';
-    if ($_POST['category'] == '2') echo '中華';
-    if ($_POST['category'] == '3') echo '洋食';
-    echo '<br>';
-    if ($_POST['difficulty'] == '1') {
-      echo '簡単';
-    } elseif ($_POST['difficulty'] == '2') {
-      echo '普通';
-    } else {
-      echo '難しい';
-    }
-    echo '<br>';
+    echo match ($_POST['category']) {
+      '1' => '和食',
+      '2' => '中華',
+      '3' => '洋食',
+    } . '<br>';
+    echo match ($_POST['difficulty']) {
+      '1' => '簡単',
+      '2' => '普通',
+      '3' => '難しい',
+    } . '<br>';
     ?>
   </body>
 </html>
