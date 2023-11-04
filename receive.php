2ed6165b75c6fc31973862f3d1302268238a892d
yujinago <yuji.yuji.0813@gmail.com>
2023-11-04 17:01:45 +0900

    add_category-name

--- receive.php
+++ receive.php
@@ -1,4 +1,18 @@
-<?php
-print_r($_POST);
-echo htmlspecialchars($_POST['recipe_name'], ENT_QUOTES);
-?>
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>出力結果</title>
+  </head>
+  <body>
+    <?php
+    // print_r($_POST);
+    echo htmlspecialchars($_POST['recipe_name'], ENT_QUOTES);
+    echo '<br>';
+    if ($_POST['category'] == '1') echo '和食';
+    if ($_POST['category'] == '2') echo '中華';
+    if ($_POST['category'] == '3') echo '洋食';
+    echo '<br>';
+    ?>
+  </body>
+</html>
