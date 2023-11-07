8918bcbc989faa46c814a2d3a425a5cdae6d54e8
yujinago <yuji.yuji.0813@gmail.com>
2023-11-07 18:02:48 +0900

    add_list.php_try-catch

--- list.php
+++ list.php
@@ -1,11 +1,16 @@
 <?php
 $user = '◯◯◯';
 $pass = '◯◯◯';
-$dbh = new PDO('mysql:host=localhost;dbname=db1;charset=utf8', $user, $pass);
-$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$sql = 'SELECT * FROM recipes';
-$stmt = $dbh->query($sql);
-$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-print_r($result);
-$dbh = null;
+try {
+  $dbh = new PDO('mysql:host=localhost;dbname=db1;charset=utf8', $user, $pass);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = 'SELECT * FROM recipes';
+  $stmt = $dbh->query($sql);
+  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  print_r($result);
+  $dbh = null;
+} catch (PDOException $e) {
+  echo 'エラー発生: ' . htmlspecialchars($e->getMessage(), ENT_QUOTES) . '<br>';
+  exit;
+}
 ?>
