339a5b0db5cf85c670cfbb299562b2046b1f7cbd
yujinago <yuji.yuji.0813@gmail.com>
2023-11-13 17:51:28 +0900

    add_detail.php_db

--- detail.php
+++ detail.php
@@ -7,7 +7,15 @@ if (empty($_GET['id'])) {
 }
 try {
   $id = (int)$_GET['id'];
-  var_dump($id);
+  $dbh = new PDO('mysql:host=localhost;dbname=db1;charset=utf8', $user, $pass);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = 'SELECT * FROM recipes WHERE id = ?';
+  $stmt = $dbh->prepare($sql);
+  $stmt->bindValue(1, $id, PDO::PARAM_INT);
+  $stmt->execute();
+  $result = $stmt->fetch(PDO::FETCH_ASSOC);
+  print_r($result);
+  $dbh = null;
 } catch (PDOException $e) {
   echo 'エラー発生: ' . htmlspecialchars($e->getMessage(), ENT_QUOTES) . '<br>';
   exit;
