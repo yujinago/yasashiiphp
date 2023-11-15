a9ff212b46133fdf5c393a6eef8cdb82f0ff9642
yujinago <yuji.yuji.0813@gmail.com>
2023-11-15 19:23:49 +0900

    create_add.php

---
+++ add.php
@@ -0,0 +1,8 @@
+<?php
+$user = '';
+$pass = '';
+$recipe_name = $_POST['recipe_name'];
+$howto = $_POST['howto'];
+$category = (int)$_POST['category'];
+$difficulty = (int)$_POST['difficulty'];
+$budget = (int)$_POST['budget'];
