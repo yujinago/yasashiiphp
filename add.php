8c7fe14da9f6d6af9dfb52c0b659ac2ada28955c
yujinago <yuji.yuji.0813@gmail.com>
2023-11-15 19:37:10 +0900

    add_add.php_sql

--- add.php
+++ add.php
@@ -6,3 +6,20 @@ $howto = $_POST['howto'];
 $category = (int)$_POST['category'];
 $difficulty = (int)$_POST['difficulty'];
 $budget = (int)$_POST['budget'];
+try {
+  $dbh = new PDO('mysql:host=localhost;dbname=db1;charset=utf8', $user, $pass);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "INSERT INTO recipes (recipe_name, category, difficulty, budget, howto) VALUES (?, ?, ?, ?, ?)";
+  $stmt = $dbh->prepare($sql);
+  $stmt->bindValue(1, $recipe_name, PDO::PARAM_STR);
+  $stmt->bindValue(2, $category, PDO::PARAM_INT);
+  $stmt->bindValue(3, $difficulty, PDO::PARAM_INT);
+  $stmt->bindValue(4, $budget, PDO::PARAM_INT);
+  $stmt->bindValue(5, $howto, PDO::PARAM_STR);
+  $stmt->ececute();
+  $dbh = null;
+  echo'レシピの登録が完了しました。';
+} catch (PDOException $e) {
+  echo 'エラー発生: ' . htmlspecialchars($e->getMessage(), ENT_QUOTES) . '<br>';
+  exit;
+}
