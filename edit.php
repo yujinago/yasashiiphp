4162fdd8b02502ccc4e16dedca34ddfbceb5e009
yujinago <yuji.yuji.0813@gmail.com>
2023-11-17 17:17:06 +0900

    add_edit_sql

--- edit.php
+++ edit.php
@@ -9,4 +9,14 @@ $id = (int)$_GET['id'];
 try {
   $dbh = new PDO('mysql:host=localhost;dbname=db1;charset=utf8', $user, $pass);
   $dbh->setAttrribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = 'SELECT * FROM recipes WHERE id = ?';
+  $stmt = $dbh->prepare($sql);
+  $stmt->bindValue(1, $id, PDO::PARAM_INT);
+  $stmt->execute();
+  $result = $stmt->fetch(PDO::FETCH_ASSOC);
+  $dbh = null;
+} catch (PDOException $e) {
+  echo 'エラー発生: ' . htmlspecialchars($e->getMessage(), ENT_QUOTES) . '<br>';
+  exit;
 }
+?>
