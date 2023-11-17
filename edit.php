7c3583137cd266be5ebe0e73b5b7b0449105e782
yujinago <yuji.yuji.0813@gmail.com>
2023-11-17 17:34:54 +0900

    add_edit_html

--- edit.php
+++ edit.php
@@ -20,3 +20,23 @@ try {
   exit;
 }
 ?>
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>入力フォーム</title>
+  </head>
+  <body>
+    レシピの投稿<br>
+    <form method="post" action="update.php?id=<?= htmlspecialchars($result['id'], ENT_QUOTES) ?>">
+      料理名：<input type="text" name="recipe_name" value="<?php echo htmlspecialchars($result['recipe_name'], ENT_QUOTES); ?>"><br>
+      カテゴリ：
+      <select name="category">
+        <option hidden>選択してください</option>
+        <option value="1" <?php if ($result['category'] == 1) echo 'selected' ?>>和食</option>
+        <option value="2" <?php if ($result['category'] == 2) echo 'selected' ?>>中華</option>
+        <option value="3" <?php if ($result['category'] == 3) echo 'selected' ?>>洋食</option>
+      </select><br>
+    </form>
+  </body>
+</html>
